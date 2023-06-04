431f4aeec84ddafaef1fcbe4241ffb6da16d8a06
Marcos_Neiva <103659504+Marcos1701@users.noreply.github.com>
2023-06-04 13:54:11 -0300

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,27 +1,4 @@
-// import express from 'express';
 import cors from 'cors';
-
-// export const app = express();
-
-
-
-// app.use(express.json());
-// app.use(express.raw({ type: 'application/vnd.custom-type' }));
-// app.use(express.text({ type: 'text/html' }));
-
-// // Healthcheck endpoint
-// app.get('/', (req, res) => {
-//   res.status(200).send({ status: 'ok' });
-// });
-
-// const api = express.Router();
-
-// api.get('/hello', (req, res) => {
-//   res.status(200).send({ message: 'hello world' });
-// });
-
-// // Version the api
-// app.use('/api/v1', api);
 
 import express, { Application, Request, Response } from 'express';
 import router from './router.js';
@@ -29,9 +6,12 @@ import router from './router.js';
 export const app: Application = express();
 app.use(cors({ origin: true }));
 
+
+app.use(express.json())
+app.use(express.raw({ type: 'application/vnd.custom-type' }));
+app.use(express.text({ type: 'text/html' }));
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'));
-app.use(express.json())
 app.use(router);
 
 app.get('/', (req: Request, res: Response) => {
