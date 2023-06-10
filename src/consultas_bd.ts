7ce2b7c8e68f1586b181da607ee2ed61f567e0c4
Marcos1701 <catce.2022111tads0428@aluno.ifpi.edu.br>
2023-06-10 20:45:04 -0300

    hehe

--- src/consultas_bd.ts
+++ src/consultas_bd.ts
@@ -312,8 +312,8 @@ export async function curtirPostagem(req: Request, res: Response) {
 
         const retorno = await client.query(`
         SELECT CURTIR_POSTAGEM('${id}', '${token}')`)
-        console.log(retorno[0].curtir_postagem)
-        const likes = retorno[0].curtir_postagem
+        console.log(retorno[0])
+        const likes = retorno[0]
         res.status(200).json({ "likes": likes });
 
     } catch (err) {
